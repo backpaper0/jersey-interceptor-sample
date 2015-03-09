15e2b983381d0e277c1e21892afd2ccd006676f5
Taichi Uragami <backpaper0@gmail.com>
2015-03-09 23:01:31 +0900

    Applicationサブクラスにコメントを追加

--- src/main/java/sample/SampleApplication.java
+++ src/main/java/sample/SampleApplication.java
@@ -2,10 +2,19 @@ package sample;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+/**
+ * Applicationサブクラス。
+ * Jerseyに依存すること前提なのでJerseyに用意されているResourceConfigをextendsしちゃう。
+ *
+ */
 public class SampleApplication extends ResourceConfig {
 
     public SampleApplication() {
+        //リソースクラスを登録する。
         register(SampleResource.class);
+
+        //TransactionInterceptorを適用するためのBinderを登録する。
+        //BinderはClassではなくインスタンスを登録する必要がある。
         register(new TransactionInterceptorBinder());
     }
 }
